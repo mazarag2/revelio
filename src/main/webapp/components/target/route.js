0cfe58cc4b431a11a8c14fbdee3e0033bc1dbcda
mazarag2 <mazarag2@gmail.com>
2020-04-30 16:04:36 -0700

    add target-list / editor

---
+++ src/main/webapp/components/target/route.js
@@ -0,0 +1,383 @@
+import { useMutation, useQuery } from '@apollo/react-hooks'
+import Dialog from '@material-ui/core/Dialog'
+import LinearProgress from '@material-ui/core/LinearProgress'
+import gql from 'graphql-tag'
+import React, { Fragment, useState } from 'react'
+import {
+  Actions,
+  AddCardItem,
+  DeleteAction,
+  IndexCardItem,
+  IndexCards,
+  ReadOnly,
+  ShareAction,
+} from '../index-cards'
+import {
+  ConfirmDeleteMetacardInteraction,
+  EditMetacardInteraction,
+  MetacardInteractionsDropdown,
+  ShareMetacardInteraction,
+} from '../index-cards/metacard-interactions'
+import RetryComponent from '../network-retry/snackbar-retry'
+import { Notification } from '../notification/notification'
+import TargetList from './target-list'
+
+const Loading = () => {
+  return <LinearProgress />
+}
+
+const AddItem = props => {
+  const { onCreate, Editor } = props
+
+  const [editing, setEditing] = useState(false)
+
+  const onSave = data => {
+    setEditing(false)
+    onCreate(data)
+  }
+
+  const onCancel = () => {
+    setEditing(false)
+  }
+
+  return (
+    <Fragment>
+      {editing ? (
+        <Dialog open>
+          <Editor onSave={onSave} onCancel={onCancel} />
+        </Dialog>
+      ) : null}
+      <AddCardItem onClick={() => setEditing(true)} />
+    </Fragment>
+  )
+}
+
+const Item = props => {
+  const { form, Editor, onDelete } = props
+  const [editing, setEditing] = useState(false)
+
+  const onSave = data => {
+    setEditing(false)
+    props.onSave(data)
+  }
+
+  const onCancel = () => {
+    setEditing(false)
+  }
+
+  return (
+    <Fragment>
+      {editing ? (
+        <Dialog open>
+          <Editor form={form} onSave={onSave} onCancel={onCancel} />
+        </Dialog>
+      ) : null}
+      <IndexCardItem {...form} onClick={() => setEditing(true)}>
+        <Actions attributes={form}>
+          <ShareAction
+            id={form.id}
+            title={form.title}
+            metacardType="attribute-group"
+          />
+          <DeleteAction itemName="target list" onDelete={onDelete} />
+          <ReadOnly indexCardType="Target List" />
+          <MetacardInteractionsDropdown>
+            <ShareMetacardInteraction
+              id={form.id}
+              title={form.title}
+              metacardType="attribute-group"
+            />
+            <EditMetacardInteraction
+              itemName="Target List"
+              onEdit={() => setEditing(true)}
+            />
+            <ConfirmDeleteMetacardInteraction
+              itemName="Target List"
+              onDelete={onDelete}
+            />
+          </MetacardInteractionsDropdown>
+        </Actions>
+      </IndexCardItem>
+    </Fragment>
+  )
+}
+
+export const Route = props => {
+  const [message, setMessage] = useState(null)
+
+  const {
+    loading,
+    error,
+    forms,
+    Editor,
+    onCreate,
+    onSave,
+    onDelete,
+    refetch,
+  } = props
+
+  if (loading) {
+    return <Loading />
+  }
+
+  if (error) {
+    return (
+      <RetryComponent
+        message={'Issue retrieving Target Lists, would you like to retry?'}
+        onRetry={refetch}
+        error={error}
+      />
+    )
+  }
+
+  return (
+    <IndexCards>
+      {message ? (
+        <Notification message={message} onClose={() => setMessage(null)} />
+      ) : null}
+      <AddItem
+        Editor={Editor}
+        onCreate={(...args) => {
+          onCreate(...args)
+          setMessage('Target List Created')
+        }}
+      />
+      {forms
+        .concat()
+        .sort((a, b) => (a.modified > b.modified ? -1 : 1))
+        .map(form => {
+          return (
+            <Item
+              key={form.id}
+              Editor={Editor}
+              onSave={(...args) => {
+                onSave(...args)
+                setMessage('Target List Saved')
+              }}
+              onDelete={() => {
+                onDelete(form)
+                setMessage('Target List Deleted')
+              }}
+              form={form}
+            />
+          )
+        })}
+    </IndexCards>
+  )
+}
+
+// TODO: Add metacardOwner to fragment
+//
+// - When the GraphQL endpoint can return metacardOwner on save, we can
+//   add metacardOwner to this fragment.
+const fragment = gql`
+  fragment ResultFormAttributes on MetacardAttributes {
+    id
+    title
+    description
+    modified: metacard_modified
+    metacard_owner
+    security_access_individuals_read
+    security_access_individuals
+    security_access_administrators
+    security_access_groups_read
+    security_access_groups
+    attributes: ui_attribute_group
+  }
+`
+
+const resultForms = gql`
+  query ResultForms {
+    metacardsByTag(tag: "attribute-group") {
+      attributes {
+        ...ResultFormAttributes
+      }
+    }
+  }
+  ${fragment}
+`
+
+const useCreate = () => {
+  const mutation = gql`
+    mutation CreateResultForms($attrs: MetacardAttributesInput!) {
+      createMetacard(attrs: $attrs) {
+        ...ResultFormAttributes
+      }
+    }
+    ${fragment}
+  `
+
+  return useMutation(mutation, {
+    update: (cache, { data }) => {
+      const query = resultForms
+      const { metacardsByTag } = cache.readQuery({ query })
+      const attributes = metacardsByTag.attributes
+        .filter(({ id }) => id !== data.createMetacard.id)
+        .concat(data.createMetacard)
+      cache.writeQuery({
+        query,
+        data: {
+          metacardsByTag: {
+            attributes,
+            __typename: 'QueryResponse',
+          },
+        },
+      })
+    },
+  })
+}
+
+const useSave = () => {
+  const mutation = gql`
+    mutation SaveResultForms($id: ID!, $attrs: MetacardAttributesInput!) {
+      saveMetacard(id: $id, attributes: $attrs) {
+        ...ResultFormAttributes
+      }
+    }
+    ${fragment}
+  `
+  return useMutation(mutation)
+}
+
+const useDelete = () => {
+  const mutation = gql`
+    mutation DeleteResultForms($id: ID!) {
+      deleteMetacard(id: $id)
+    }
+  `
+
+  return useMutation(mutation, {
+    update: (cache, { data }) => {
+      const query = resultForms
+
+      const { metacardsByTag } = cache.readQuery({ query })
+      const attributes = metacardsByTag.attributes.filter(
+        ({ id }) => id !== data.deleteMetacard
+      )
+
+      cache.writeQuery({
+        query,
+        data: {
+          metacardsByTag: {
+            attributes,
+            __typename: 'QueryResponse',
+          },
+        },
+      })
+    },
+  })
+}
+
+const resultFormAttributes = gql`
+  query ResultFormAttributes {
+    metacardTypes {
+      id
+    }
+  }
+`
+
+export const Editor = props => {
+  const { form, attributes, loading, error, onCancel, onSave } = props
+
+  if (loading) {
+    return <Loading />
+  }
+
+  if (error) {
+    return <div>Error</div>
+  }
+
+  return (
+    <TargetList
+      style={{
+        maxHeight: '60vh',
+      }}
+      form={form}
+      attributes={attributes}
+      onCancel={onCancel}
+      onSave={onSave}
+    />
+  )
+}
+
+const EditorWithAttributes = props => {
+  const { loading, error, data } = useQuery(resultFormAttributes)
+
+  const attributes = loading ? [] : data.metacardTypes.map(({ id }) => id)
+
+  return (
+    <Editor
+      loading={loading}
+      error={error}
+      attributes={attributes}
+      {...props}
+    />
+  )
+}
+
+export default () => {
+  const { loading, error, data, refetch } = useQuery(resultForms)
+
+  const [create] = useCreate()
+  const [save] = useSave()
+  const [_delete] = useDelete()
+
+  if (loading) {
+    return <Route loading={loading} />
+  }
+
+  if (error) {
+    return <Route error={error} refetch={refetch} />
+  }
+
+  const onCreate = form => {
+    const { attributes, modified, ...rest } = form
+    create({
+      variables: {
+        attrs: {
+          ui_attribute_group: attributes,
+          metacard_type: 'attribute-group',
+          metacard_tags: ['VALID', 'attribute-group'],
+          metacard_modified: modified,
+          ...rest,
+        },
+      },
+    })
+  }
+
+  const onSave = form => {
+    const { attributes, modified, ...rest } = form
+    save({
+      variables: {
+        id: form.id,
+        attrs: {
+          ui_attribute_group: attributes,
+          metacard_modified: modified,
+          metacard_type: 'attribute-group',
+          ...rest,
+        },
+      },
+    })
+  }
+
+  const onDelete = form => {
+    _delete({
+      variables: {
+        id: form.id,
+      },
+    })
+  }
+
+  const forms = data.metacardsByTag.attributes
+
+  return (
+    <Route
+      forms={forms}
+      Editor={EditorWithAttributes}
+      onCreate={onCreate}
+      onSave={onSave}
+      onDelete={onDelete}
+      refetch={refetch}
+    />
+  )
+}
---
+++ src/main/webapp/components/target/target-list.js
@@ -0,0 +1,127 @@
+import React, { memo, useState } from 'react'
+
+import { Set } from 'immutable'
+import { useUndoState } from '../../react-hooks'
+
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+
+import { InputLabel } from '@material-ui/core';
+import { FormLabel } from '@material-ui/core';
+import Divider from '@material-ui/core/Divider';
+import SearchIcon from '@material-ui/icons/Search';
+import AddBoxIcon from '@material-ui/icons/AddBox';
+
+const validate = (form = {}) => {
+  const { title, description } = form
+
+  const attributes = Set(form.attributes)
+
+  const errors = {}
+
+  if (typeof title !== 'string') {
+    errors.title = 'Title must be string'
+  } else if (title.trim() === '') {
+    errors.title = 'Title must not be empty'
+  }
+
+  if (description !== undefined && typeof description !== 'string') {
+    errors.description = 'Description must be string'
+  }
+
+  if (attributes.isEmpty()) {
+    errors.attributes = 'Attributes cannot be empty'
+  }
+
+  return errors
+}
+
+export default props => {
+  const { form = {}, attributes = [], onCancel, onSave } = props
+
+  const { state, setState, ...rest } = useUndoState(Set(form.attributes))
+
+  const [title, setTitle] = useState(form.title || '')
+  const [description, setDescription] = useState(form.description || '')
+
+  const errors = validate({ title, description, attributes: state })
+
+  const [submitted, setSubmitted] = useState(false)
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%',
+        maxHeight: '100%',
+        padding: 20,
+        boxSizing: 'border-box',
+        ...props.style,
+      }}
+    >
+
+      <TextField
+        autoFocus
+        required
+        fullWidth
+        label="Enter a Title"
+        variant="outlined"
+        style={{ marginBottom: 20 }}
+        value={title}
+        error={submitted && errors.title !== undefined}
+        helperText={submitted ? errors.title : undefined}
+        onChange={e => setTitle(e.target.value)}
+      />
+
+      <FormLabel> Classification </FormLabel>
+      <TextField
+        fullWidth
+        rows={2}
+        multiline
+        variant="outlined"
+        label="Description"
+        style={{ marginBottom: 30 }}
+        value={description}
+        onChange={e => setDescription(e.target.value)}
+      />
+      <Divider variant="middle" />
+      <div  style={{display : 'flex'}}>
+        <TextField label="Search Attribute" defaultValue="Enter BE Number, WKT, Target Text..."  display="inline"> 
+        </TextField>
+        <Button>
+          <AddBoxIcon variant="primary" display="inline"/>
+        </Button>
+      </div>
+      
+
+      <div
+        style={{ display: 'flex', justifyContent: 'flex-end', marginTop: 20 }}
+      >
+        <Button variant="outlined" color="secondary" onClick={onCancel}>
+          Cancel
+        </Button>
+        <div style={{ width: 10, display: 'inline-block' }} />
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            if (Object.keys(errors).length === 0) {
+              const resultForm = {
+                id: form.id,
+                title,
+                description,
+                attributes: state.toJSON(),
+              }
+              onSave(resultForm)
+            } else {
+              setSubmitted(true)
+            }
+          }}
+        >
+          Save
+        </Button>
+      </div>
+    </div>
+  )
+}
